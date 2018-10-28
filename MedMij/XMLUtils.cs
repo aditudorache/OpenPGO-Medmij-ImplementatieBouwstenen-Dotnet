7525b4d8f835afe46bb3807e8092c99a1b6d6bc4
Adrian Tudorache <a.p.tudorache@gmail.com>
2018-10-28 13:39:08 +0100

    xsd files moved to separate folder, test watcher added

--- MedMij/XMLUtils.cs
+++ MedMij/XMLUtils.cs
@@ -15,7 +15,7 @@ namespace MedMij
     {
         internal static XmlSchemaSet SchemaSetFromResource(string name, XNamespace ns)
         {
-            var resourcename = $"MedMij.{name}";
+            var resourcename = $"MedMij.Xsd.{name}";
             var resource = typeof(XMLUtils).Assembly.GetManifestResourceStream(resourcename);
             if (resource == null)
             {
