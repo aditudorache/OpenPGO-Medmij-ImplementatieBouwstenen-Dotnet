b480c006682fd7c9d7e409fb1ba057e5be3d4eb7
Adrian Tudorache <a.p.tudorache@gmail.com>
2018-12-02 11:07:58 +0100

    add example project

---
+++ MedMij.Example/Demo.cs
@@ -0,0 +1,14 @@
+// Copyright (c) Adrian Tudorache. All Rights Reserved. Licensed under the AGPLv3 (see License.txt for details).
+
+using System;
+
+namespace MedMij.Example
+{
+    public static class Demo
+    {
+        public static void Run()
+        {
+            Console.WriteLine("Run!");
+        }
+    }
+}
---
+++ MedMij.Example/Program.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Adrian Tudorache. All Rights Reserved. Licensed under the AGPLv3 (see License.txt for details).
+
+using System;
+
+namespace MedMij.Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start demo consumers for OpenPGO-Medmij-DotNet!");
+            Demo.Run();
+        }
+    }
+}
